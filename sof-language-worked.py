a832841fb88624ad5475d59015b219793c6bba4e
atishayshukla <atishayshukla@gmail.com>
2019-10-20 12:52:07 +0200

    Final implementation to get the percentage

--- sof-language-worked.py
+++ sof-language-worked.py
@@ -3,14 +3,20 @@ from collections import defaultdict, Counter
 
 with open('data/survey_results_public.csv', encoding='utf8') as f:
     csv_reader = csv.DictReader(f)
+    total = 0
 
     # counter comes to rescue for things like counting and is intelligent as well
     language_counter = Counter()
 
     for line in csv_reader:
         languages = line['LanguageWorkedWith'].split(';')
-        for language in languages:
-            # Now we need to create counter for this
-            language_counter[language] += 1
-        print(language_counter)
-        break
+        # instead of adding language_counter[language] += 1 we can use update method
+        language_counter.update(languages)
+        total += 1
+
+# To print 5 most common language
+for language, value in language_counter.most_common(5):
+    language_pct = (value / total) * 100
+    language_pct = round(language_pct, 2)
+
+    print(f'{language}: {language_pct}%')
